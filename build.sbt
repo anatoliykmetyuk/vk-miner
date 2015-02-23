adab83249253876f8c60e1f8d7c28839ff508eee
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-02-24 01:30:59 +0200

    k-means clustering using Apache Spark. Code is a mess...

--- build.sbt
+++ build.sbt
@@ -9,15 +9,26 @@ initialCommands := """
   |import org.json4s.native.JsonMethods._
   |import org.json4s.JsonDSL._
   |
+  |import org.apache.spark.SparkContext
+  |import org.apache.spark.SparkContext._
+  |import org.apache.spark.SparkConf
+  |
+  |import StringMethods._
+  |
   |implicit val formats = org.json4s.DefaultFormats
   |
   |val token = "167a139a663b46cee17b96828bf71d9ed57975e4e576546237560cf9c235a4c921b892366ccbbbd95ece2"
   |
   |def analyzer(name: String) = new Analyzer(token, name)
+  |
+  |val sc = new SparkContext(new SparkConf().setAppName("I own it!").setMaster("local"))
+  |val clusterization = new Clusterization(sc)
+  |import clusterization._
 """.stripMargin
 
 libraryDependencies ++= Seq(
   "commons-io" % "commons-io" % "2.4",
   "org.apache.httpcomponents" % "httpclient" % "4.4",
-  "org.json4s" %% "json4s-native" % "3.2.11"
+  "org.json4s" %% "json4s-native" % "3.2.11",
+  "org.apache.spark" %% "spark-mllib" % "1.2.1"
 )
---
+++ src/main/scala/vkminer/analyzer/Clusterization.scala
@@ -0,0 +1,59 @@
+package vkminer.analyzer
+
+import java.io.File
+
+import org.apache.spark.SparkContext
+import org.apache.spark.mllib.clustering._
+import org.apache.spark.mllib.linalg._
+
+import org.apache.commons.io._
+
+import StringMethods._
+
+
+class Clusterization(val sc: SparkContext) {
+
+  def clusterize(docs: Seq[String], k: Int = 15, vectorSize: Int = 300, iterations: Int = 1000): Seq[(String, Int)] = {
+    // 1. Extract words from the documents
+    val words = extractWords(docs)
+
+    // 2. Create a bag of the most popular words
+    val frequencies = wordFrequencies(words)
+    val bag         = wordsBag(frequencies.map(_._2), vectorSize)
+
+    // 3. Turn every document into a vector
+    val activeDocs = docs.filter {_.split(" ").exists(bag.contains(_))}  // Each document must contain at least one word from the bag
+    
+    val docVectors = activeDocs.map {doc =>
+      val vectorData: Seq[(Int, Double)] = doc
+        .split(" ")                // Words
+        .distinct                  
+        .filter(bag.contains(_))   // Only those that are in the bag
+        .map {w => bag(w) -> 1D}   // Turn each word into a pair of its index and 1
+
+      Vectors.sparse(vectorSize, vectorData)
+    }
+
+    val docRDD = sc.makeRDD(docVectors)
+
+    // 4. Train K-Means, predict the payload
+    val model = KMeans.train(docRDD, k, iterations)
+    println(s"Model generated with the error ${model.computeCost(docRDD)}")
+
+    val clusters = model.predict(docRDD).collect
+
+    // 5. Zip the original docs with their clusters
+    activeDocs.zip(clusters)
+  }
+
+  def clustersToMap(c: Seq[(String, Int)]): Map[Int, Seq[String]] = c
+    .groupBy {case (n, c) => c}                      // Group by the cluster index
+    .map     {case (c, seq) => c -> seq.map(_._1)}   // Remove indices
+
+  def serializeClusters(c: Map[Int, Seq[String]], dir: String, fileName: String = "clusters.txt") {
+    val raw: Seq[Seq[String]] = c.map(_._2).toSeq
+    val result: String = raw.map(_.mkString("\n")).mkString("\n\n\n=== NEW CLUSTER ===\n\n\n")
+    FileUtils.writeStringToFile(new File(dir, fileName), result)
+  }
+
+}

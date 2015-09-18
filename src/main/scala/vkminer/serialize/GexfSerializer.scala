4ccca00e6e9c802d212fe69a0e0810edbd380662
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2015-09-19 01:00:53 +0300

    Weights as edge labels

--- src/main/scala/vkminer/serialize/GexfSerializer.scala
+++ src/main/scala/vkminer/serialize/GexfSerializer.scala
@@ -81,7 +81,7 @@ trait GexfSerializerComponent extends SerializerComponent {this: VkEnvironment =
             </nodes>
             <edges>
               {graph.edges.zipWithIndex.map {case (Edge(sid, tid, w), i) =>
-                <edge id={i.toString} source={sid} target={tid} weight={w.toString}/>
+                <edge id={i.toString} source={sid} target={tid} weight={w.toString} label={w.toString}/>
               }}
             </edges>
           </graph>
